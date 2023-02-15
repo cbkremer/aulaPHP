a00104a491d31545a996b575a7deb865d5e553fa
Carlos Bianchi Kremer <182110075@senacinformatica.br>
2023-02-15 14:18:33 -0300

    conexão com o banco ok

--- controller/CUsuario.php
+++ controller/CUsuario.php
@@ -8,15 +8,14 @@ class CUsuario{
             $usuario = $_POST['usuario'];
             $senha = $_POST['senha'];
             $perfilAcesso = $_POST['perfilAcesso'];
-
+            
             $pdo = require_once '../pdo/Connection.php';
-            $sql = "insert into usuario ('idUsuario','nomeUsuario','usuario','senha','perfilAcesso')
-            values (null,?,?,?,?)";
+            $sql = "insert into usuario values (null,?,?,?,?)";
             $sth = $pdo->prepare($sql);
             $sth->bindParam(1, $nome,PDO::PARAM_STR);
             $sth->bindParam(2, $usuario,PDO::PARAM_STR);
-            $sth->bindParam(3, $senhaEc,PDO::PARAM_STR);
-            $sth->bindParam(4, $perfilAcesso,PDO::PARAM_STR);
+            $sth->bindParam(3, $perfilAcesso,PDO::PARAM_STR);
+            $sth->bindParam(4, $senhaEc,PDO::PARAM_STR);
             $senhaEc = password_hash($senha,PASSWORD_DEFAULT);
             $sth->execute();
             unset($sth);
--- pdo/Connection.php
+++ pdo/Connection.php
@@ -6,7 +6,7 @@ require_once('Config.php');
 class Connection{
     public static function getConnection($host, $dbName, $userName, $pass){
         //era pra ser dsnkkkk
-        $dns = "mysql:host=$host;dbName=$dbName;charset=UTF8";
+        $dns = "mysql:host=$host;dbname=$dbName;charset=UTF8";
         try{
             $option = [PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION];
             return new PDO($dns,$userName,$pass,$option);
